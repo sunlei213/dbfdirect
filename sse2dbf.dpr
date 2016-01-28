b875b0f66c4bebfc540b0c09a4ab32cf641f5145
sunlei213 <sunleity@163.com>
2016-01-28 16:16:33 +0800

    程序project

--- sse2dbf.dpr
+++ sse2dbf.dpr
@@ -4,7 +4,8 @@ uses
   Vcl.Forms,
   sse2dbf_main in 'sse2dbf_main.pas' {Form2},
   ArrayEx in 'ArrayEx.pas',
-  DBFdirect in 'DBFdirect.pas';
+  DBFdirect in 'DBFdirect.pas',
+  sse2dbf_set in 'sse2dbf_set.pas' {settaskentry};
 
 {$R *.res}
 
@@ -12,5 +13,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm2, Form2);
+//  Application.CreateForm(Tsettaskentry, settaskentry);
   Application.Run;
 end.
