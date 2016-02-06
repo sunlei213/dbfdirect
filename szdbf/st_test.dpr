f6630b196a36ce7f9652eebf3f50d0489aeb1a9a
sunlei213 <sunleity@163.com>
2016-02-06 19:14:47 +0800

    sz_test项目

--- szdbf/st_test.dpr
+++ szdbf/st_test.dpr
@@ -3,7 +3,9 @@ program st_test;
 uses
   Vcl.Forms,
   sz_test_main in 'sz_test_main.pas' {MYform},
-  sz_fix in 'sz_fix.pas';
+  sz_fix in 'sz_fix.pas',
+  sz_interface in 'sz_interface.pas',
+  DBFdirect in 'DBFdirect.pas';
 
 {$R *.res}
 
