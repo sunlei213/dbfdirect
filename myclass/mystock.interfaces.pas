2a118abbf9489beb31eb400fb4dca9955def8f30
sunlei213 <sunleity@163.com>
2016-02-24 15:28:10 +0800

    接口定义

--- myclass/mystock.interfaces.pas
+++ myclass/mystock.interfaces.pas
@@ -32,13 +32,13 @@ type
 	  function GetDeci:Byte;
 	  function format(obj:variant):string;
 	  function parse(s:string):variant;
-	  procedure SetName(na:string);
+	  procedure SetName(na:AnsiString);
 	  procedure SetType(ty:ansichar);
-	  procedure SetLength(le:integer);
-	  procedure SetDeci(deci:integer);
-    property name:AnsiString read SetName write SetName;
-    property Field_type:AnsiChar read SetType write SetType;
-    property length:Byte read SetLength write SetLength;
+	  procedure SetLength(le:Byte);
+	  procedure SetDeci(deci:Byte);
+    property name:AnsiString read GetName write SetName;
+    property Field_type:AnsiChar read GetType write SetType;
+    property length:Byte read GetLength write SetLength;
     property dec:Byte read GetDeci write SetDeci;
 
   end;
@@ -58,5 +58,38 @@ type
     function getdate(body_ln: UInt32): Boolean; virtual; abstract;
   end;
 implementation
+{ Tstock }
+
+
+constructor Tstock.Create(AClient: TIdTCPClient; i: UInt32);
+var
+  il: uin32;
+begin
+  Self.AClient := AClient;
+  il.i32 := i;
+  chk := il.by32[3] + il.by32[2] + il.by32[1] + il.by32[0];
+end;
+
+
+procedure Tstock.recvbuff;
+var
+  I, j: Integer;
+begin
+  j := Length(self.tby);
+  Self.AClient.IOHandler.ReadBytes(self.tby, j, False);
+  for I := 0 to j - 1 do
+    Self.chk := Self.chk + self.tby[i];
+end;
+
+function Tstock.Serial(query: TQueue<tarrayex<Variant>>; body_ln: UInt32): Boolean;
+begin
+  if getdate(body_ln) then
+  begin
+    query.Enqueue(data_stream);
+    Result := True;
+  end
+  else
+    Result := False;
+end;
 
 end.
