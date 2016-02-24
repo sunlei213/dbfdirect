b4d0291f156b671e54c216d822b4127582f7d405
sunlei213 <sunleity@163.com>
2016-02-24 15:27:35 +0800

    类定义

--- myclass/mystock.classes.pas
+++ myclass/mystock.classes.pas
@@ -1,14 +1,11 @@
 unit mystock.classes;
-
 interface
-uses
-  mystock.types,mystock.interfaces,IdGlobal,IdTCPClient,Generics.Collections, ArrayEx, System.SysUtils, System.Variants, System.Classes,system.Math;
-type
 
-  TExtFuns = class(Tobject)
-  public
-    class function IfThen<T>(AValue: Boolean; const ATrue, AFalse: T): T; inline;
-  end;
+uses
+  mystock.types, mystock.interfaces, IdGlobal, IdTCPClient, Generics.Collections,
+  ArrayEx, System.SysUtils, System.Variants, System.Classes, system.Math;
+
+type
 
   Tstock_hq = class(Tstock)
   private
@@ -59,13 +56,11 @@ type
 
   TSjshqWrite = class(TInterfacedObject, Idata_trans)
   private
-    max_cov_num:Integer;
+    max_cov_num: Integer;
   protected
-
   public
-    constructor Create(cover_num:Integer);
-    function cover_data(d_map:TDictionary<string,tarrayex<variant>>;
-      queue:TQueue<tarrayex<Variant>>):Integer;
+    constructor Create(cover_num: Integer);
+    function cover_data(d_map: TDictionary<string, tarrayex<variant>>; queue: TQueue<tarrayex<Variant>>): Integer;
     function write_dbf(filename: string): Boolean;
   end;
 
@@ -73,43 +68,13 @@ type
 
   TSjsxxWrite = class(TInterfacedObject, Idata_trans)
   private
-    max_cov_num:Integer;
+    max_cov_num: Integer;
   protected
-
   public
-    constructor Create(cover_num:Integer);
-    function cover_data(d_map:TDictionary<string,tarrayex<variant>>;queue:TQueue<tarrayex<Variant>>):Integer;
-    function write_dbf(filename:string):Boolean;
-
+    constructor Create(cover_num: Integer);
+    function cover_data(d_map: TDictionary<string, tarrayex<variant>>; queue: TQueue<tarrayex<Variant>>): Integer;
+    function write_dbf(filename: string): Boolean;
   end;
-{ TDBField }
-
-  TDBField = class(TInterfacedObject, IDBField)
-  private
-	  fname:ansistring;
-	  fField_type:ansichar;
-	  flength,fdec:integer;
-  protected
-
-  public
-	  constructor CreateFromField(fiel:DBField);
-	  constructor Create(na:string;ty:ansichar;le,deci:integer);
-    function GetName:AnsiString;
-	  function GetType:ansichar;
-	  function GetLength:Byte;
-	  function GetDeci:Byte;
-	  function format(obj:variant):string;
-	  function parse(s:string):variant;
-	  procedure SetName(na:string);
-	  procedure SetType(ty:ansichar);
-	  procedure SetLength(le:integer);
-	  procedure SetDeci(deci:integer);
-    property name:AnsiString read SetName write SetName;
-    property Field_type:AnsiChar read SetType write SetType;
-    property length:Byte read GetLength write SetLength;
-    property dec:Byte read GetDeci write SetDeci;
-  end;
-
 
 implementation
 { Tstock_hq }
@@ -170,6 +135,7 @@ begin
   chk := chk mod 256;
   Result := (l = chk);
 end;
+
 
 
 { Tstock_zs }
@@ -286,34 +252,34 @@ end;
 
 { TSjshqWrite }
 
-function TSjshqWrite.cover_data(d_map: TDictionary<string, tarrayex<variant>>;
-  queue: TQueue<tarrayex<Variant>>): Integer;
+function TSjshqWrite.cover_data(d_map: TDictionary<string, tarrayex<variant>>; queue: TQueue<tarrayex<Variant>>): Integer;
 var
-  my_data,tmp_data:TArrayEx<Variant>;
-  I,j,k:Integer;
+  my_data, tmp_data: TArrayEx<Variant>;
+  I, j, k: Integer;
 begin
-  j:=Min(max_cov_num ,queue.Count);
-  Result:=0;
-  for I := 0 to j-1 do
+  j := Min(max_cov_num, queue.Count);
+  Result := 0;
+  for I := 0 to j - 1 do
+  begin
+    if queue.Count = 0 then
+      Break;
+    my_data := queue.Dequeue;
+    if d_map.ContainsKey(my_data[0]) then
     begin
-      if queue.Count=0 then Break;
-      my_data:=queue.Dequeue;
-      if d_map.ContainsKey(my_data[0]) then
-        begin
-          tmp_data:=d_map.Items[my_data[0]];
-          for k := 0 to my_data.Len-1  do
-            tmp_data[k]:=my_data[k];
-        end;
-
-      Result:=Result+1;
-
+      tmp_data := d_map.Items[my_data[0]];
+      for k := 0 to my_data.Len - 1 do
+        tmp_data[k] := my_data[k];
     end;
+
+    Result := Result + 1;
+
+  end;
 end;
 
 constructor TSjshqWrite.Create(cover_num: Integer);
 begin
   inherited Create;
-  max_cov_num:=cover_num;
+  max_cov_num := cover_num;
 end;
 
 function TSjshqWrite.write_dbf(filename: string): Boolean;
@@ -323,8 +289,7 @@ end;
 
 { TSjsxxWrite }
 
-function TSjsxxWrite.cover_data(d_map: TDictionary<string, tarrayex<variant>>;
-  queue: TQueue<tarrayex<Variant>>): Integer;
+function TSjsxxWrite.cover_data(d_map: TDictionary<string, tarrayex<variant>>; queue: TQueue<tarrayex<Variant>>): Integer;
 begin
 
 end;
@@ -332,306 +297,12 @@ end;
 constructor TSjsxxWrite.Create(cover_num: Integer);
 begin
   inherited Create;
-  max_cov_num:=cover_num;
+  max_cov_num := cover_num;
 end;
 
 function TSjsxxWrite.write_dbf(filename: string): Boolean;
 begin
 end;
 
-{ TExtFuns }
-
-class function TExtFuns.IfThen<T>(AValue: Boolean; const ATrue, AFalse: T): T;
-begin
-  if AValue then
-    Result := ATrue
-  else
-    Result := AFalse;
-end;
-
-//------------------------------------------------------------------------------
-// ×¢ÊÍ
-//------------------------------------------------------------------------------
-
-constructor TDBField.Create(na:string;ty:ansichar;le,deci:integer);
-begin
-  if (system.Length(na)>10) then
-  raise Exception.Create('The field name is more than 10 characters long:'+na );
-
-  if (ty <> 'C') and (ty <> 'N') and (ty <> 'L') and (ty <> 'D') and (ty <> 'F') then
-  raise Exception.Create('The field type is not a valid. Got: '+ty);
-
-  if (le<1) then
-  raise Exception.Create('The field length should be a positive integer. Got: '+inttostr(le));
-
-  if (ty = 'C') and (le>=254) then
-  raise Exception.Create('The field length should be less than 254 characters for character fields. Got: '+inttostr(le));
-
-  if (ty = 'N') and (le>=21) then
-  raise Exception.Create('The field length should be less than 21 digits for numeric fields. Got: '+inttostr(le));
-
-  if (ty = 'L') and (le<>1) then
-  raise Exception.Create('The field length should be 1 characater for logical fields. Got: '+inttostr(le));
-
-  if (ty = 'D') and (le<>8) then
-  raise Exception.Create('The field length should be 8 characaters for date fields. Got: '+inttostr(le));
-
-  if (ty = 'F') and (le>=21) then
-  raise Exception.Create('The field length should be less than 21 digits for floating point fields. Got: '+inttostr(le));
-
-  if (deci < 0) then
-  raise Exception.Create('The field decimal count should not be a negative integer. Got: '+inttostr(deci));
-
-  if ((ty = 'C') or (ty = 'L') or (ty = 'D')) and (deci <> 0) then
-  raise Exception.Create('The field decimal count should be 0 for character, logical, and date fields. Got: '+inttostr(deci));
-
-  if (deci > (le-1)) then
-  raise Exception.Create('The field decimal count should be less than the length - 1. Got: '+inttostr(deci));
-
-  self.fname:=ansistring(na);
-  self.fField_type:=ty;
-  self.flength:=le;
-  self.fdec:=deci;
-end;
-
-constructor TDBField.CreateFromField(fiel: DBField);
-var
-  na:ansistring;
-  ty:ansichar;
-  le,deci:integer;
-begin
-  na:=pansichar(@(fiel.Fieldname));
-  ty:=fiel.FieldType;
-  le:=fiel.FieldLenth;
-  deci:=fiel.FieldDec;
-  if (system.Length(na)>10) then
-  raise Exception.Create('The field name is more than 10 characters long:'+string(na) );
-
-  if (ty <> 'C') and (ty <> 'N') and (ty <> 'L') and (ty <> 'D') and (ty <> 'F') then
-  raise Exception.Create('The field type is not a valid. Got: '+ty);
-
-  if (le<1) then
-  raise Exception.Create('The field length should be a positive integer. Got: '+inttostr(le));
-
-  if (ty = 'C') and (le>=254) then
-  raise Exception.Create('The field length should be less than 254 characters for character fields. Got: '+inttostr(le));
-
-  if (ty = 'N') and (le>=21) then
-  raise Exception.Create('The field length should be less than 21 digits for numeric fields. Got: '+inttostr(le));
-
-  if (ty = 'L') and (le<>1) then
-  raise Exception.Create('The field length should be 1 characater for logical fields. Got: '+inttostr(le));
-
-  if (ty = 'D') and (le<>8) then
-  raise Exception.Create('The field length should be 8 characaters for date fields. Got: '+inttostr(le));
-
-  if (ty = 'F') and (le>=21) then
-  raise Exception.Create('The field length should be less than 21 digits for floating point fields. Got: '+inttostr(le));
-
-  if (deci < 0) then
-  raise Exception.Create('The field decimal count should not be a negative integer. Got: '+inttostr(deci));
-
-  if ((ty <> 'C') or (ty <> 'L') or (ty <> 'D')) and (deci <> 0) then
-  raise Exception.Create('The field decimal count should be 0 for character, logical, and date fields. Got: '+inttostr(deci));
-
-  if (deci > (le-1)) then
-  raise Exception.Create('The field decimal count should be less than the length - 1. Got: '+inttostr(deci));
-
-  self.fname:=na;
-  self.fField_type:=fiel.FieldType;
-  self.flength:=fiel.FieldLenth;
-  self.fdec:=fiel.FieldDec;
-end;
-
-function TDBField.format(obj: Variant):string;
-begin
-  if ((self.fField_type='N') or (self.fField_type='F')) and ( VarIsClear(obj) or varisnull(obj))then
-  begin
-    Result:= getNumberNullValue();
-    exit;
-  end;
-  Result:=formatValue(obj);
-end;
-
-function TDBField.formatValue(obj: Variant):string;
-var
-  lowdec1,i,j:integer;
-  st1,st2:ansistring;
-  ob:string;
-  bo:boolean;
-  re:Extended;
-begin
-  if (self.fField_type='N') or (self.fField_type='F') then
-  begin
-    if (vartype(obj)=varString) or (vartype(obj)=varUString) then
-    begin
-      ob:=obj;
-      i:=System.Length(ob);
-      re:=StrToFloat(ob);
-      lowdec1:=self.dec;
-      if (re>=10000.0) and (self.fdec>0) then lowdec1:=lowdec1-1;
-      st1:=ansistring(FloatToStrF(re,ffFixed,i,lowdec1));
-      i:= self.Length-system.Length(st1);
-      if i<0 then raise Exception.Create('Value ' + string(st1) + ' cannot fit in pattern');
-      system.SetLength(st2,i);
-      for j := 1 to i do st2[j]:=' ';
-      Result:=string(st2+st1);
-      exit;
-    end;
-    raise Exception.Create('Field Type Eror for ' + obj + '.');
-  end;
-  if (self.fField_type='C') then
-  begin
-    if varisnull(obj) or VarIsClear(obj) then
-    begin
-      system.SetLength(st1,self.getlength);
-      for I := 1 to self.getlength do st1[i]:=' ';
-      obj:=st1;
-    end;
-    if (vartype(obj)=varString) or (vartype(obj)=varUString) then
-    begin
-      ob:=obj;
-      st1:=ansistring(ob);
-      if (system.Length(st1)>self.getLength) then raise Exception.Create('"' + obj +
-      '" is longer than ' + inttostr(self.getLength) + ' characters, value: ' + obj);
-      system.SetLength(st2,self.getLength-system.Length(st1));
-      for I := 1 to self.getLength-system.Length(st1) do st2[i]:=' ';
-      Result:=string(st2+st1);
-      exit;
-    end;
-    raise Exception.Create('Expected a String, got '+inttostr(VarType(obj))
-    +', value: '+vartostr(obj));
-  end;
-  if (self.fField_type='L') then
-  begin
-    if varisnull(obj) or VarIsClear(obj) then
-    begin
-      obj:=false;
-    end;
-    if (vartype(obj)=varBoolean) then
-    begin
-      bo:=obj;
-      Result:=TExtFuns.IfThen(bo,'Y','N');
-      exit;
-    end;
-    raise Exception.Create('Expected a Boolean, got '+VarTypeAsText(VarType(obj))
-    +', value: '+vartostr(obj));
-  end;
-  if (self.fField_type='D') then
-  begin
-    if varisnull(obj) or VarIsClear(obj) then
-    begin
-      obj:=Date;
-    end;
-    if (vartype(obj)=varDate) then
-    begin
-      Result:=formatdatetime('yyyymmdd',variants.VarToDateTime(obj));
-      exit;
-    end;
-    raise Exception.Create('Expected a Date, got '+VarTypeAsText(VarType(obj))
-    +', value: '+vartostr(obj));
-  end;
-  raise Exception.Create('Unrecognized JDBFField type: '+VarTypeAsText(VarType(obj)));
-end;
-
-function TDBField.getDeci: Byte;
-begin
-  Result:=self.fdec;
-end;
-
-function TDBField.getLength: Byte;
-begin
-  Result:=self.flength;
-end;
-
-function TDBField.GetName: AnsiString;
-begin
-  Result:=self.fname;
-end;
-
-function TDBField.getNumberNullValue: string;
-var
-st1,st2:TstringBuilder;
-i,j:integer;
-begin
-  st1:=TStringBuilder.create;
-  st2:=TStringBuilder.create;
-  if self.getLength>0 then st1.append('-');
-  if self.getDeci>0 then
-  begin
-    st1.append('.');
-    st1.append('-',self.fdec);
-//    for I := 0 to self.getDeci - 1 do st1:=st1+'-';
-  end;
-  i:=self.getLength-system.Length(st1);
-  st2.append(' ',i);
-//  for j := 0 to i - 1 do st2:=st2+' ';
-  Result:=st2.tostring+st1.tostring;
-end;
-
-function TDBField.GetType: ansichar;
-begin
-  Result:=self.fField_type;
-end;
-
-function TDBField.parse(s: string): variant;
-var
-st1:string;
-df1:TFormatSettings;
-
-begin
-  if (self.fField_type='N') or (self.fField_type='F') then
-  begin
-    st1:=stringreplace(s,'.','', [rfReplaceAll]);
-    st1:=stringreplace(st1,'-','', [rfReplaceAll]);
-    if st1='' then exit(s);
-    try
-    begin
-       if self.getDeci=0 then exit(StrToInt(s));
-       exit(strtofloat(s));
-    end;
-    except on E: EConvertError do raise E;
-    end;
-  end;
-  if (self.fField_type='C') then exit(s);
-  if (self.fField_type='L') then
-  begin
-    if ((s='Y') or (s='y') or (s='T') or (s='t')) then exit(True);
-    if ((s='N') or (s='n') or (s='F') or (s='f')) then exit(false);
-    raise Exception.Create('Unrecognized value for logical field: '+s);
-  end;
-  if (self.fField_type='D') then
-  begin
-    df1.ShortDateFormat:='yyyymmdd';
-    try
-      begin
-        if s='' then exit(null);
-        exit(strtodate(s,df1));
-      end;
-    except on E: EConvertError do raise E;
-    end;
-  end;
-  raise Exception.Create('Unrecognized JDBFField type: '+self.Field_type);
-end;
-
-procedure TDBField.SetDeci(deci: integer);
-begin
-  self.fdec:=Byte(deci);
-end;
-
-procedure TDBField.SetLength(le: integer);
-begin
-  self.flength:=Byte(le);
-end;
-
-procedure TDBField.SetName(na: string);
-begin
-  self.fname:=ansistring(na);
-end;
-
-procedure TDBField.SetType(ty: ansichar);
-begin
-  self.fField_type:=ty;
-end;
 
 end.
