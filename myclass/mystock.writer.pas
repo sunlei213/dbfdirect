cdf3d6bf92bfb8edc83aba93641ffa006c1c631c
sunlei213 <sunleity@163.com>
2016-03-02 23:06:37 +0800

    写数据类

--- myclass/mystock.writer.pas
+++ myclass/mystock.writer.pas
@@ -18,6 +18,7 @@ type
     function getfreg:Integer;
     procedure setfreg(sec:Integer);
     procedure writedbf;virtual;
+    procedure initfield;virtual;abstract;
   public
     constructor Create;
     procedure setpath(path:string);
@@ -33,15 +34,17 @@ type
 
   TSJSHQ_wr=class(TMy_Writer)
   protected
-    procedure writedbf;override;
+    procedure initfield;override;
+
   public
     constructor Create;
     procedure init_data;override;
     destructor Destroy; override;
-
   end;
 
 implementation
+uses
+  mystock.singleinf.logger;
 
 { TMy_Writer }
 
@@ -106,9 +109,50 @@ begin
 end;
 
 procedure TMy_Writer.writedbf;
+var
+  stl: TList<string>;
+  st1: string;
+  write1: IDBFwrite;
+  obj1: TArrayEx<Variant>;
+  delflag: Boolean;
+  logger: ILogger;
 begin
+  stl := TList<string>.Create(Self.fmap.Keys);
+  write1 := TDBFWrite.Create(ffields);
+  write1.initHead2Stream(Self.fmap.Count);
+  logger := GetLogInterface;
+  try
+    begin
+      stl.Sort;
+      MonitorEnter(fmap);
+      try
+        for st1 in stl do
+        begin
+          obj1 := Self.fmap.Items[st1];
+          delflag := False;
+          write1.addRecord(delflag, obj1);
+        end;
+      finally
+        MonitorExit(fmap);
+      end;
+      try
+        write1.wirteStream2File(Self.fFilename);
+      except
+        on E: Exception do
+          logger.WriteLog('Œƒº˛%s–¥»Î ß∞‹£¨¥ÌŒÛ¿‡£∫%s,¥ÌŒÛ‘≠“Ú%s', [self.fFilename, e.ClassName,
+            e.Message], 2);
+      end;
+    end;
+//  for st1 in stl do Self.datamap.Items[st1]:=nil;
+//  Self.datamap.Clear;
+  finally
+    stl.Free;
+    write1 := nil;
+    logger := nil;
+  end;
 
 end;
+
 
 { TSJSHQ_wr }
 
@@ -124,59 +168,74 @@ begin
   inherited;
 end;
 
-procedure TSJSHQ_wr.init_data;
-
+procedure TSJSHQ_wr.initfield;
 begin
-    fFilename:=fpath+'\sjshq.dbf';
-    if FileExists(ffileName) then
-    begin
-      //∂¡»°hq◊÷∂Œ
-    end
-    else
-    begin
-      ffields.Add(TDBField.Create('HQZQDM', 'C', 6, 0));
-      ffields.Add(TDBField.Create('HQZQJC', 'C', 8, 0));
-      ffields.Add(TDBField.Create('HQZRSP', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQJRKP', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQZJCJ', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQCJSL', 'N', 12, 0));
-      ffields.Add(TDBField.Create('HQCJJE', 'N', 17, 3));
-      ffields.Add(TDBField.Create('HQCJBS', 'N', 9, 0));
-      ffields.Add(TDBField.Create('HQZGCJ', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQZDCJ', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQSYL1', 'N', 7, 2));
-      ffields.Add(TDBField.Create('HQSYL2', 'N', 7, 2));
-      ffields.Add(TDBField.Create('HQJSD1', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQJSD2', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQHYCC', 'N', 12, 0));
-      ffields.Add(TDBField.Create('HQSJW5', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQSSL5', 'N', 12, 0));
-      ffields.Add(TDBField.Create('HQSJW4', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQSSL4', 'N', 12, 0));
-      ffields.Add(TDBField.Create('HQSJW3', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQSSL3', 'N', 12, 0));
-      ffields.Add(TDBField.Create('HQSJW2', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQSSL2', 'N', 12, 0));
-      ffields.Add(TDBField.Create('HQSJW1', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQSSL1', 'N', 12, 0));
-      ffields.Add(TDBField.Create('HQBJW1', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQBSL1', 'N', 12, 0));
-      ffields.Add(TDBField.Create('HQBJW2', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQBSL2', 'N', 12, 0));
-      ffields.Add(TDBField.Create('HQBJW3', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQBSL3', 'N', 12, 0));
-      ffields.Add(TDBField.Create('HQBJW4', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQBSL4', 'N', 12, 0));
-      ffields.Add(TDBField.Create('HQBJW5', 'N', 9, 3));
-      ffields.Add(TDBField.Create('HQBSL5', 'N', 12, 0));
+  ffields.Add(TDBField.Create('HQZQDM', 'C', 6, 0));
+  ffields.Add(TDBField.Create('HQZQJC', 'C', 8, 0));
+  ffields.Add(TDBField.Create('HQZRSP', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQJRKP', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQZJCJ', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQCJSL', 'N', 12, 0));
+  ffields.Add(TDBField.Create('HQCJJE', 'N', 17, 3));
+  ffields.Add(TDBField.Create('HQCJBS', 'N', 9, 0));
+  ffields.Add(TDBField.Create('HQZGCJ', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQZDCJ', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQSYL1', 'N', 7, 2));
+  ffields.Add(TDBField.Create('HQSYL2', 'N', 7, 2));
+  ffields.Add(TDBField.Create('HQJSD1', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQJSD2', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQHYCC', 'N', 12, 0));
+  ffields.Add(TDBField.Create('HQSJW5', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQSSL5', 'N', 12, 0));
+  ffields.Add(TDBField.Create('HQSJW4', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQSSL4', 'N', 12, 0));
+  ffields.Add(TDBField.Create('HQSJW3', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQSSL3', 'N', 12, 0));
+  ffields.Add(TDBField.Create('HQSJW2', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQSSL2', 'N', 12, 0));
+  ffields.Add(TDBField.Create('HQSJW1', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQSSL1', 'N', 12, 0));
+  ffields.Add(TDBField.Create('HQBJW1', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQBSL1', 'N', 12, 0));
+  ffields.Add(TDBField.Create('HQBJW2', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQBSL2', 'N', 12, 0));
+  ffields.Add(TDBField.Create('HQBJW3', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQBSL3', 'N', 12, 0));
+  ffields.Add(TDBField.Create('HQBJW4', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQBSL4', 'N', 12, 0));
+  ffields.Add(TDBField.Create('HQBJW5', 'N', 9, 3));
+  ffields.Add(TDBField.Create('HQBSL5', 'N', 12, 0));
 
-    end;
 end;
 
-procedure TSJSHQ_wr.writedbf;
+procedure TSJSHQ_wr.init_data;
+var
+  dbfread: IDBFRead;
+  readtrue: Boolean;
 begin
-
-
+  fFilename := fpath + '\sjshq.dbf';
+  if FileExists(ffileName) then
+  begin
+      //∂¡»°hq◊÷∂Œ
+    dbfread := TDBFWrite.Create(ffields);
+    try
+      readtrue := dbfread.ReadFile2Stream(fFilename);
+    except
+      on E: Exception do
+      begin
+        initfield;
+        Exit;
+      end;
+    end;
+    if readtrue then
+      dbfread.initStream2Head
+    else
+      initfield;
+  end
+  else
+  begin
+    initfield;
+  end;
 end;
 
 end.
